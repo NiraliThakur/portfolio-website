4e284774f4b9b3f69cf19049fc33eecb71c1e519
Nirali Thakur <147072479+NiraliThakur@users.noreply.github.com>
2024-10-20 13:36:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const typingAnimationElement = document.getElementById('typing-text');
+
+const typingTexts = ['Full-stack Developer ! ',
+];
+
+function playTypingAnimation(text) {
+    for (let i = 0; i< text.length; i++) {
+        setTimeout(() => {
+            typingAnimationElement.textContent += text[i];
+            }, i*200);
+    }
+
+    setTimeout(() => {
+        typingAnimationElement.textContent = '';
+        playTypingAnimation(typingTexts[(typingTexts.indexOf(text) + 1) % typingTexts.length]);
+
+    }, text.length * 200);
+}
+
+playTypingAnimation(typingTexts[0]);
